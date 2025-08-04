ac075f600c804d7d22313db1ba447f2700dd765e
tarikkatasoy <157570991+tarikkatasoy@users.noreply.github.com>
2025-08-03 22:46:43 -0700

    feat: Apply all mentor feedback for M2 logic

--- hello_world/app.py
+++ hello_world/app.py
@@ -2,41 +2,56 @@ import json
 import os
 import uuid
 import boto3
+import logging
+import validators
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
-# Initialize the DynamoDB client
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table(os.environ.get("TABLE_NAME"))
+short_url_table = dynamodb.Table(os.environ.get("URL_LOOKUP_TABLE_NAME_DYNAMODB"))
 
 
 def lambda_handler(event, context):
-   try:
-       # Get the long URL from the POST request body
-       body = json.loads(event.get("body", "{}"))
-       long_url = body.get("long_url")
+   logger.info(f"Received event: {event}")
 
+   try:
+
+       body = json.loads(event.get("body", "{}"))
+       long_url = body.get("url")
 
        if not long_url:
+           logger.error("Validation Error: The 'long_url' field was not found in the request body.")
+
            return {
                "statusCode": 400,
-               "body": json.dumps({"error": "long_url is a required field"}),
+                "body": json.dumps({"error": "No URL found in the request."}),
+           }
+       if not validators.url(long_url):
+           logger.error(f"Validation Error: The provided URL is not valid. URL: '{long_url}'")
+           return {
+               "statusCode": 400,
+               "body": json.dumps({"error": "The provided URL is not valid."}),
            }
 
 
-       # Generate a unique short ID (I will replace this with base62 later)
-       short_id = str(uuid.uuid4())[:7]
+       # TODO (tarik): Replace this with base62 logic
+       short_id = uuid.uuid4().hex[:7]
 
 
-       # Save the mapping to DynamoDB
-       table.put_item(
-           Item={
-               'id': short_id,
-               'long_url': long_url
-           }
+ 
+       item_to_save = {
+           'id': short_id,
+           'long_url': long_url
+       }
+       
+       logger.info(f"Saving new item to DynamoDB: {item_to_save}")
+ 
+       short_url_table.put_item(
+           Item=item_to_save
        )
 
-
-       # Construct the short URL to return to the user
        api_gateway_url = f"https://{event['requestContext']['domainName']}/{event['requestContext']['stage']}"
        short_url = f"{api_gateway_url}/{short_id}"
 
@@ -49,9 +64,9 @@ def lambda_handler(event, context):
            }),
        }
    except Exception as e:
-       print(f"Error: {e}")
+       logger.error(f"An unexpected error occurred: {e}")
        return {
            "statusCode": 500,
            "body": json.dumps({"error": "An internal error occurred"}),
        }
-
+   
